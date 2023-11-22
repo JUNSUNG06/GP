8c86ed8098c7e3938460e91dae62a2e5aed493ab
JUNSUNG06 <forest2847@gmail.com>
2023-11-22 09:28:32 +0900

    rigidbody class

--- 2023_winapi_framework/2023_winapi_framework/Rigidbody.cpp
+++ 2023_winapi_framework/2023_winapi_framework/Rigidbody.cpp
@@ -1,2 +1,35 @@
 #include "pch.h"
 #include "Rigidbody.h"
+#include "TimeMgr.h"
+
+Rigidbody::Rigidbody(Object* _pOwner)
+	: m_pOwner(nullptr)
+	, m_fGravityScale(-9.81f)
+	, m_fBounciness(0)
+	, m_bUseGravity(true)
+	, m_bReverseGravity(false)
+	, m_vecVelocity({0, 0})
+{
+}
+
+Rigidbody::~Rigidbody()
+{
+}
+
+void Rigidbody::FinalUpdate()
+{
+	if (m_bUseGravity == true)
+	{
+		m_vecVelocity.y += m_fGravityScale * fDT;
+	}
+}
+
+void Rigidbody::SetReverseGravity(bool _bReverseGravity)
+{
+	m_bReverseGravity = _bReverseGravity;
+
+	m_fGravityScale = abs(m_fGravityScale);
+
+	if (m_bReverseGravity == false)
+		m_fGravityScale *= -1;
+}
--- 2023_winapi_framework/2023_winapi_framework/Rigidbody.h
+++ 2023_winapi_framework/2023_winapi_framework/Rigidbody.h
@@ -8,19 +8,24 @@ public:
 	~Rigidbody();
 
 public:
+	void FinalUpdate();
+public:
 	Object* GetOwner() { return m_pOwner; }
-	float GetGravity() { return m_fGravity; }
-	void SetGravity(float _fGravity) { m_fGravity = _fGravity; }
-	void SetUserGravity(bool _bValue) { m_bUseGravity = _bValue; }
+	float GetGravityScale() { return m_fGravityScale; }
+	void SetGravityScale(float _fGravity) { m_fGravityScale = _fGravity; }
+	bool GetUseGravity() { return m_bUseGravity; }
+	void SetUseGravity(bool _bValue) { m_bUseGravity = _bValue; }
 	float GetBounciness() { return m_fBounciness; }
 	void SetBounciness(float _fBounciness) { m_fBounciness = _fBounciness; }
 	Vec2 GetVelocity() { return m_vecVelocity; }
-	Vec2 GetVelocity() { return m_vecVelocity; }
+	void SetVelocity(Vec2 _vecVelocity) { m_vecVelocity = _vecVelocity; }
+	bool GetReverseGravity() { return m_bReverseGravity; }
+	void SetReverseGravity(bool _bReverseGravity);
 private:
 	Object* m_pOwner;
-	float m_fGravity;
-	bool m_bUseGravity;
+	float m_fGravityScale;
 	float m_fBounciness;
+	bool m_bUseGravity;
+	bool m_bReverseGravity;
 	Vec2 m_vecVelocity;
 };
-
