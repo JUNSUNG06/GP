2cff47402d40b6dd04246c3bed55336b5779ffdb
9mean3 <gminseong114@gmail.com>
2023-11-27 00:56:27 +0900

    플레잉어 기반 완성하였습니다. 설명 참고.
    현재의 프로젝트에서 생기는 ResMgr오류로 인하여 다른 프로젝트에서 테스트를 진행하였습니다.
    사실 스크립트 한 개로 플레이어를 제작할 수 있었겠지만 뭔가 어려울 것 같아서 이렇게 시작하였는데 다시 생각해보면 이게 훨씬 어려운 방법으로 보입니다.
    내일 수정할 수 있도록 하겠습니다.
    
    현재 다른 프로젝트에서 테스트 하였을 때, 플레이어의 기본적인 좌우 움직임, 일정 시간 후 총알 발사, 총알이 적을 향하는 것 등이 되었습니다. 총알은 원래 프로젝트에 있었던 총알을 그대로 사용하였습니다.

--- 2023_winapi_framework/2023_winapi_framework/Player1.cpp
+++ 2023_winapi_framework/2023_winapi_framework/Player1.cpp
@@ -3,13 +3,18 @@
 #include "KeyMgr.h"
 #include "TimeMgr.h"
 #include "Collider.h"
+#include "Bullet.h"
+#include "SceneMgr.h"
+#include "Scene.h"
 
 Player1::Player1()
 	: m_pTex(nullptr)
-	, int m_iHP(1)
-	, float m_fPlayerSpeed(0f)
-	, float m_fFireDelay(0f)
-	, float m_fBulletSpeed(0f)
+	, m_iHP(1)
+	, m_fPlayerSpeed(100.f)
+	, m_fFireDelay(0.f)
+	, m_fCurFireDelay(3.f)
+	, m_fBulletSpeed(3.f)
+	, m_pEnemy(nullptr)
 {
 	CreateCollider();
 	GetCollider()->SetScale(Vec2(20.f, 30.f));
@@ -23,17 +28,24 @@ void Player1::Update()
 {
 	Vec2 vPos = GetPos();
 
-	if (KEY_PRESS(KEY_TYPE::A))
+	if (KEY_PRESS(KEY_TYPE::LEFT))
 	{
 		vPos.x -= m_fPlayerSpeed * fDT;
 		//GetAnimator()->PlayAnim(L"Jiwoo_Left", true);
 	}
-	if (KEY_PRESS(KEY_TYPE::D))
+	if (KEY_PRESS(KEY_TYPE::RIGHT))
 	{
 		vPos.x += m_fPlayerSpeed * fDT;
 		//GetAnimator()->PlayAnim(L"Jiwoo_Right", true);
 	}
 	SetPos(vPos);
+
+
+	m_fCurFireDelay += fDT;
+	if (m_fCurFireDelay >= m_fFireDelay) {
+		Attack();
+		m_fCurFireDelay = 0;
+	}
 }
 
 void Player1::Render(HDC _dc)
@@ -43,4 +55,14 @@ void Player1::Render(HDC _dc)
 
 void Player1::Attack()
 {
+	Bullet* pBullet = new Bullet;
+	Vec2 vBulletPos = GetPos();
+	pBullet->SetPos(vBulletPos);
+	pBullet->SetScale(Vec2(25.f, 25.f));
+	//	pBullet->SetDir(M_PI / 4 * 7);
+	//	pBullet->SetDir(120* M_PI / 180);
+	//pBullet->SetDir(Vec2(-10.f, -15.f));
+	pBullet->SetDir({ m_pEnemy->GetPos().x - GetPos().x, m_pEnemy->GetPos().y - GetPos().y });
+	pBullet->SetName(L"Player_Bullet");
+	SceneMgr::GetInst()->GetCurScene()->AddObject(pBullet, OBJECT_GROUP::BULLET);
 }
--- 2023_winapi_framework/2023_winapi_framework/Player2.cpp
+++ 2023_winapi_framework/2023_winapi_framework/Player2.cpp
@@ -3,13 +3,18 @@
 #include "KeyMgr.h"
 #include "TimeMgr.h"
 #include "Collider.h"
+#include "Bullet.h"
+#include "SceneMgr.h"
+#include "Scene.h"
 
 Player2::Player2()
 	: m_pTex(nullptr)
-	, int m_iHP(1)
-	, float m_fPlayerSpeed(0f)
-	, float m_fFireDelay(0f)
-	, float m_fBulletSpeed(0f)
+	, m_iHP(1)
+	, m_fPlayerSpeed(100.f)
+	, m_fFireDelay(0.f)
+	, m_fCurFireDelay(3.f)
+	, m_fBulletSpeed(3.f)
+	, m_pEnemy(nullptr)
 {
 	CreateCollider();
 	GetCollider()->SetScale(Vec2(20.f, 30.f));
@@ -34,6 +39,13 @@ void Player2::Update()
 		//GetAnimator()->PlayAnim(L"Jiwoo_Right", true);
 	}
 	SetPos(vPos);
+
+
+	m_fCurFireDelay += fDT;
+	if (m_fCurFireDelay >= m_fFireDelay) {
+		Attack();
+		m_fCurFireDelay = 0;
+	}
 }
 
 void Player2::Render(HDC _dc)
@@ -43,4 +55,14 @@ void Player2::Render(HDC _dc)
 
 void Player2::Attack()
 {
+	Bullet* pBullet = new Bullet;
+	Vec2 vBulletPos = GetPos();
+	pBullet->SetPos(vBulletPos);
+	pBullet->SetScale(Vec2(25.f, 25.f));
+	//	pBullet->SetDir(M_PI / 4 * 7);
+	//	pBullet->SetDir(120* M_PI / 180);
+	//pBullet->SetDir(Vec2(-10.f, -15.f));
+	pBullet->SetDir({ m_pEnemy->GetPos().x - GetPos().x, m_pEnemy->GetPos().y - GetPos().y });
+	pBullet->SetName(L"Player_Bullet");
+	SceneMgr::GetInst()->GetCurScene()->AddObject(pBullet, OBJECT_GROUP::BULLET);
 }
