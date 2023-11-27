de24da90c0a078172a84069aec545a334bbb3f52
9mean3 <gminseong114@gmail.com>
2023-11-27 14:18:18 +0900

    Player crystal

--- 2023_winapi_framework/2023_winapi_framework/Player1.h
+++ 2023_winapi_framework/2023_winapi_framework/Player1.h
@@ -10,11 +10,20 @@ public:
 public:
     void Update() override;
     void Render(HDC _dc) override;
+    void SetPlayerSpeed(float _speed) { m_fPlayerSpeed = _speed; }
+    void SetFireDelay(float _delay) { m_fFireDelay = _delay; }
     void Attack();
+    //
+    void SetEnemy(Object* _obj) { m_pEnemy = _obj; }
+    //
 private:
     Texture* m_pTex;
     int m_iHP;
     float m_fPlayerSpeed;
     float m_fFireDelay;
+    float m_fCurFireDelay;
     float m_fBulletSpeed;
+    //
+    Object* m_pEnemy;
+    //
 };
--- 2023_winapi_framework/2023_winapi_framework/Start_Scene.cpp
+++ 2023_winapi_framework/2023_winapi_framework/Start_Scene.cpp
@@ -3,38 +3,57 @@
 #include "Object.h"
 #include "Core.h"
 #include "Player.h"
+#include "Player1.h"
+#include "Player2.h"
 #include "Monster.h"
 #include "KeyMgr.h"
 #include "CollisionMgr.h"
 #include "ResMgr.h"
 void Start_Scene::Init()
 {
-	Object* pObj = new Player;
+	/*Object* pObj = new Player;
 	pObj->SetPos((Vec2({Core::GetInst()->GetResolution().x /2, Core::GetInst()->GetResolution().y / 2})));
 	pObj->SetScale(Vec2(100.f,100.f));
-	AddObject(pObj, OBJECT_GROUP::PLAYER);
+	AddObject(pObj, OBJECT_GROUP::PLAYER);*/
+
+	Player1* pObj = new Player1;
+	//pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2, (Core::GetInst()->GetResolution().y / 2) + 100 })));
+	//pObj->SetScale(Vec2(100.f, 100.f));
+	//pObj->SetPlayerSpeed(100.f);
+	//pObj->SetFireDelay(3.f);
+	//AddObject(pObj, OBJECT_GROUP::PLAYER);
+
+	//Player2* pObj1 = new Player2;
+	//pObj1->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2 })));
+	//pObj1->SetScale(Vec2(100.f, 100.f));
+	//pObj1->SetPlayerSpeed(100.f);
+	//pObj1->SetFireDelay(3.f);
+	//AddObject(pObj1, OBJECT_GROUP::PLAYER2);
+
+	//pObj->SetEnemy(pObj1);
+	//pObj1->SetEnemy(pObj);
 
 	// 몬스터 세팅 마구마구 배치를 해봅시다.
 
-	Vec2 vResolution = Core::GetInst()->GetResolution();
-	Monster* pMonster = nullptr;
-	int iMonster = 10;		// 몬스터 수 
-	float fMoveDist = 30.f; // 움직일 거리
-	float fMonsterScale = 50.f; // 몬스터 크기
-	// 해상도x - ( 움직일 거리 + 오브젝트 크기 /2) * 2 / 몬스터수 -1 
-	float fTerm = (vResolution.x - (fMoveDist + fMonsterScale / 2.f) * 2) 
-					/ (float)(iMonster -1);
-	for (int i = 0; i < iMonster; ++i)
-	{
-		pMonster = new Monster;
-		pMonster->SetPos(Vec2(
-			(fMoveDist + fMonsterScale / 2.f) + i* fTerm
-			,300.f));
-		pMonster->SetScale(Vec2(fMonsterScale, fMonsterScale));
-		pMonster->SetCenterPos(pMonster->GetPos());
-		pMonster->SetMoveDis(fMoveDist);
-		AddObject(pMonster, OBJECT_GROUP::MONSTER);
-	}
+	//Vec2 vResolution = Core::GetInst()->GetResolution();
+	//Monster* pMonster = nullptr;
+	//int iMonster = 10;		// 몬스터 수 
+	//float fMoveDist = 30.f; // 움직일 거리
+	//float fMonsterScale = 50.f; // 몬스터 크기
+	//// 해상도x - ( 움직일 거리 + 오브젝트 크기 /2) * 2 / 몬스터수 -1 
+	//float fTerm = (vResolution.x - (fMoveDist + fMonsterScale / 2.f) * 2) 
+	//				/ (float)(iMonster -1);
+	//for (int i = 0; i < iMonster; ++i)
+	//{
+	//	pMonster = new Monster;
+	//	pMonster->SetPos(Vec2(
+	//		(fMoveDist + fMonsterScale / 2.f) + i* fTerm
+	//		,300.f));
+	//	pMonster->SetScale(Vec2(fMonsterScale, fMonsterScale));
+	//	pMonster->SetCenterPos(pMonster->GetPos());
+	//	pMonster->SetMoveDis(fMoveDist);
+	//	AddObject(pMonster, OBJECT_GROUP::MONSTER);
+	//}
 	// 사운드 세팅
 	ResMgr::GetInst()->LoadSound(L"BGM", L"Sound\\Retro_bgm.wav", true);
 	ResMgr::GetInst()->LoadSound(L"Shoot", L"Sound\\laserShoot.wav", false);
